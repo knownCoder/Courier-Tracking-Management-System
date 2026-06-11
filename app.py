9a43c6ef105f61acd020568b1df9cd66c96f7204
Swapnil Gupta <swapnil.gpt04@gmail.com>
2026-06-11 23:46:53 +0530

    Prepare project for deployment

--- app.py
+++ app.py
@@ -111,5 +111,7 @@ def track_shipment():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
 
+    app.run(host="0.0.0.0", port=5000)
